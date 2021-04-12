72e7582bc0e7edc8dddc6b78a83282b4d61da922
Stuart Bailey <stubailey18@hotmail.com>
2021-04-12 19:38:40 +0100

    Populated service and service test

--- src/main/java/org/example/springapp/MovieService.java
+++ src/main/java/org/example/springapp/MovieService.java
@@ -1,2 +1,39 @@
-package org.example.springapp;public class MovieService {
+package org.example.springapp;
+
+import java.util.List;
+
+public class MovieService {
+
+    private MovieDao dao;
+
+    public MovieService(MovieDao dao) {
+        this.dao = dao;
+    }
+
+    public long add(Movie newMovie) {
+        // TODO: check newMovie is not a duplicate
+        // TODO: validate newMovie
+        return dao.add(newMovie);
+    }
+
+    public List<Movie> findByTitleLike(String partialTitle) {
+        return dao.findByTitleLike(partialTitle);
+    }
+
+    public List<Movie> findByGenre(MovieGenre genre) {
+        return dao.findByGenre(genre);
+    }
+
+    public List<Movie> findByReleaseYear(int releaseYear) {
+        return dao.findByReleaseYear(releaseYear);
+    }
+
+    public void update(Movie updatedMovie) throws NoSuchMovieException {
+        // TODO: validate updatedMovie
+        dao.update(updatedMovie);
+    }
+
+    public void delete(Movie movieToDelete) throws NoSuchMovieException {
+        dao.delete(movieToDelete);
+    }
 }
--- src/test/java/org/example/springapp/MovieServiceTest.java
+++ src/test/java/org/example/springapp/MovieServiceTest.java
@@ -1,2 +1,25 @@
-package org.example.springapp;public class MovieServiceTest {
+package org.example.springapp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.example.springapp.MovieGenre.COMEDY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MovieServiceTest {
+
+    private MovieService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new MovieService(new MovieDao());
+    }
+
+    @Test
+    public void testAdd() throws NoSuchMovieException {
+        long id = service.add(new Movie("Wayne's World", COMEDY, 1992));
+        assertEquals("Wayne's World", service.findByTitleLike("Wayne").get(0).getTitle());
+    }
+
+    // TODO: write more tests
 }
