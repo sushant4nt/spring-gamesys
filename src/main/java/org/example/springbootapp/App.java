a715212e59e5c194a15a9f0310c0e1073c277bf0
Stuart Bailey <stubailey18@hotmail.com>
2021-04-13 21:19:21 +0100

    Tidied up

--- src/main/java/org/example/springbootapp/App.java
+++ src/main/java/org/example/springbootapp/App.java
@@ -1,12 +1,4 @@
 package org.example.springbootapp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class App {
-
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
 }
--- src/test/java/org/example/springbootapp/AppTest.java
+++ src/test/java/org/example/springbootapp/AppTest.java
@@ -1,7 +1,4 @@
 package org.example.springbootapp;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 public class AppTest {
 }
