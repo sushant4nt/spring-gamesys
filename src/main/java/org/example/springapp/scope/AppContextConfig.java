30d0d759cf11bc1e5871db11f79427cff6d759e9
Stuart Bailey <stubailey18@hotmail.com>
2021-04-13 12:30:53 +0100

    Complete scope example

--- src/main/java/org/example/springapp/scope/AppContextConfig.java
+++ src/main/java/org/example/springapp/scope/AppContextConfig.java
@@ -1,4 +1,21 @@
 package org.example.springapp.scope;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
 public class AppContextConfig {
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public PrototypeBean prototypeBean() {
+        return new PrototypeBean();
+    }
+
+    @Bean
+    public SingletonBean singletonBean() {
+        return new SingletonBean();
+    }
 }
--- src/test/java/org/example/springapp/scope/BeanScopeTest.java
+++ src/test/java/org/example/springapp/scope/BeanScopeTest.java
@@ -1,4 +1,33 @@
 package org.example.springapp.scope;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 public class BeanScopeTest {
+
+    private ApplicationContext context;
+
+    @BeforeEach
+    public void setUp() {
+        context = new AnnotationConfigApplicationContext(AppContextConfig.class);
+    }
+
+    @Test
+    public void testPrototypeScopeBean() {
+        PrototypeBean p1 = context.getBean(PrototypeBean.class);
+        PrototypeBean p2 = context.getBean(PrototypeBean.class);
+        assertNotSame(p1, p2);
+    }
+
+    @Test
+    public void testSingletonBean() {
+        SingletonBean s1 = context.getBean(SingletonBean.class);
+        SingletonBean s2 = context.getBean(SingletonBean.class);
+        assertSame(s1, s2);
+    }
 }
