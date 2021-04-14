3456378232f23a35fa2c24826ae340237dbbb82e
Stuart Bailey <stubailey18@hotmail.com>
2021-04-14 14:34:46 +0100

    Complete boot example

--- src/main/java/org/example/springbootapp/PropertyResolver.java
+++ src/main/java/org/example/springbootapp/PropertyResolver.java
@@ -1,6 +1,7 @@
 package org.example.springbootapp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
