3781b47429c2ea3a765ff47dd37ff206469ed6ff
Stuart Bailey <stubailey18@hotmail.com>
2021-04-15 20:41:11 +0100

    Added POM deps and MyController

---
+++ src/main/java/org/example/springbootapp/MyController.java
@@ -0,0 +1,16 @@
+package org.example.springbootapp;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+public class MyController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
